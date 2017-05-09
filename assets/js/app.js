3cea2c2abe997641b25d86e1d8fe31ca35e567be
GracielaCesareo <graciela.cesareoc@gmail.com>
2017-05-09 16:33:06 -0500

    agregada funcion para ocultar/mostrar texto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+//Funcion para ocultar imagenes
 var imagenes = document.getElementsByClassName ("imgContainer");
 
 for (var i = 0; i < imagenes.length; i++) {
@@ -6,5 +7,41 @@ for (var i = 0; i < imagenes.length; i++) {
 
 function ocultar() {
   this.style.display = "none";
+}
 
+
+//Funcion para ocultar/mostrar texto
+var txtIzq = document.getElementById('izquierda');
+var txtDer = document.getElementById('derecha');
+var botonOrigen = document.getElementById('origen');
+var botonExtincion = document.getElementById('extincion');
+
+//Esta funcion cuenta los clicks que serán comparados despues saber qué hará el texto
+var clicks = 0;
+function onClick() {
+    clicks += 1;
+}
+botonOrigen.addEventListener("click", onClick);
+botonExtincion.addEventListener("click", onClick);
+
+//Funcion para ocultar/mostar texto lado izquierdo
+botonOrigen.addEventListener("click", removerIzq);
+
+function removerIzq () {
+  if (clicks % 2 != 0) {
+    txtIzq.style.visibility = "hidden";
+  } else if (clicks % 2 == 0) {
+    txtIzq.style.visibility = "visible";
+  }
+}
+
+//Funcion para ocultar/mostar texto lado izquierdo
+botonExtincion.addEventListener("click", removerDer);
+
+function removerDer () {
+  if (clicks % 2 != 0) {
+    txtDer.style.visibility = "hidden";
+  } else if (clicks % 2 == 0) {
+    txtDer.style.visibility = "visible";
+  }
 }
